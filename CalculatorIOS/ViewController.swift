f1128ee3c6ed499a90d83d72ab587ebff10f46c0
jaw233 <jaw233@txstate.edu>
2016-05-27 08:52:40 -0500

    Initial Commit :/

---
+++ CalculatorIOS/ViewController.swift
@@ -0,0 +1,127 @@
+//
+//  ViewController.swift
+//  CalculatorIOS
+//
+//  Created by Jared Walton on 5/26/16.
+//  Copyright © 2016 Jared Walton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // instance of the class "calcInput" which holds all the methods for calculation.
+    var calc = calcInput()
+    var hasInput1: Bool = false
+    var hasInput2: Bool = false
+    var input: String = " " //holds the input from view
+    var prevOp: String = " "
+    var input1: Double = 0
+    var input2: Double = 0
+    var opCount: Int = 0
+
+    //text field outlet
+    @IBOutlet weak var calcTextField: UITextView!
+    
+    // update text view on button click
+    @IBAction func updateText(sender: AnyObject) {
+        
+        // get the button "id" as a string
+        input = ((sender as! UIButton).titleLabel!.text!)
+        
+        // number was input so append it to view
+        if (Double(input) != nil){
+            self.calcTextField.text = self.calcTextField.text + input
+        }
+        
+        // operation was input
+        if (Double(input) == nil){
+            
+            // handle the case that are not "resuable logic"
+            if (input == "C"){ clearCalc(); return}
+            if (input == "."){ self.calcTextField.text = self.calcTextField.text + "."; return }
+            if (input == "%"){
+                self.calcTextField.text = calc.inputInterpretation("%", input1: Double(self.calcTextField.text)!, input2: 0)
+                return
+            }
+            if (input == "N") { self.calcTextField.text = "-" + self.calcTextField.text; return }
+            
+            // set up the input
+            if (hasInput1 == true) {
+                if (Double(self.calcTextField.text) != nil){
+                    input2 = Double(self.calcTextField.text)!
+                    hasInput2 = true
+                }
+            }
+            if (hasInput1 == false){
+                if (Double(self.calcTextField.text) != nil){
+                    input1 = Double(self.calcTextField.text)!
+                    self.calcTextField.text = ""
+                    hasInput1 = true
+                }
+            }
+            
+            opCount += 1
+            if (opCount == 2){
+                if (input == "="){
+                    self.calcTextField.text = calc.inputInterpretation(prevOp, input1: input1, input2: input2)
+                }else{
+                    self.calcTextField.text = calc.inputInterpretation(input, input1: input1, input2: input2)
+                }
+                opCount -= 2
+                hasInput1 = false
+                hasInput2 = false
+            }
+            prevOp = input
+        }
+
+    }
+    
+    //reset everything!
+    func clearCalc(){
+        self.calcTextField.text = ""
+        input1 = 0; input2 = 0; opCount = 0;
+        hasInput1 = false; hasInput2 = false;
+        prevOp = " "; input = " "
+        calc.reset()
+    }
+}
+
+
+
+
+// if it is a number append it to the text view.
+//if (Double(input) != nil){
+//    //            if (resultReturned == true) { self.calcTextField.text = ""; resultReturned = false; input1 = 0}
+//    self.calcTextField.text = self.calcTextField.text + input
+//    needsSecondinput = false
+//}
+
+//// if it is an operation.. perform the operation.
+//if (Double(input) == nil && nextShouldBeNumber == false) {
+//    if (input == "C"){
+//        clearCalc()
+//    }else if (input == "."){
+//        self.calcTextField.text = self.calcTextField.text + "."
+//    }else else if (input == "="){
+//        self.calcTextField.text = String(calc.getValue())
+//    }else{
+//        if (input1 == 0){
+//            input1 = Double(self.calcTextField.text)!
+//            self.calcTextField.text = ""
+//        }
+//        else {
+//            if (needsSecondinput == false){
+//                input2 = Double(self.calcTextField.text)!
+//                if (input == "="){
+//                    input1 = Double(calc.inputInterpretation(prevOp, input1: input1, input2: input2))!
+//                }else{
+//                    input1 = Double(calc.inputInterpretation(input, input1: input1, input2: input2))!
+//                }
+//                resultReturned = true
+//                needsSecondinput = true
+//                self.calcTextField.text = String(input1)
+//            }
+//        }
+//    }
+//}
---
+++ CalculatorIOS/calcInput.swift
@@ -0,0 +1,51 @@
+//
+//  calcInput.swift
+//  CalculatorIOS
+//
+//  Created by Jared Walton on 5/26/16.
+//  Copyright © 2016 Jared Walton. All rights reserved.
+//
+
+import Foundation
+
+
+class calcInput {
+    
+    var value: Double = 0;
+    
+    
+    // methods for calculations
+    private func mod(input: Double, input2: Double) { self.value = input % input2 }
+    private func percent(input: Double) { self.value = input / 100 }
+    private func add(input: Double, input2: Double) { self.value = input + input2 }
+    private func sub(input: Double, input2: Double) { self.value = input - input2}
+    private func mult(input: Double, input2: Double){ self.value = input * input2 }
+    private func div(input: Double, input2: Double) { self.value = input / input2 }
+    private func negate(input: Double) { self.value = -input}
+    
+    
+    // determine the operation.
+    internal func inputInterpretation(operation: String, input1: Double, input2: Double) -> String{
+        
+        if (operation == "M"){ mod(input1, input2: input2) }
+        if (operation == "%")  { percent(input1) }
+        if (operation == "+")  { add(input1, input2: input2) }
+        if (operation == "-")  { sub(input1, input2: input2) }
+        if (operation == "X")  { mult(input1, input2: input2) }
+        if (operation == "/")  { div(input1, input2: input2) }
+        if (operation == "N"){ negate(input1) }
+        
+        print(String(input1) + operation + String(input2) + " = " + String(self.value))
+        
+        
+        return String(self.value)
+    }
+    
+    
+    
+    func reset(){ self.value = 0 }
+    
+    // getters and setters
+    func getValue() -> Double { return value }
+    func setValue(input: Double) { value = input }
+}
